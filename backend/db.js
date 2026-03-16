5ab5209b5f91b1e747891298f59d6f19f73eec36
Kunal <killerkunal45@gmail.com>
2026-03-16 15:03:33 +0000

    Database configured
    using mongoose and mongodb

--- backend/db.js
+++ backend/db.js
@@ -1,12 +1,35 @@
 const mongoose = require("mongoose")
 const { Schema } = require("zod")
 
-mongoose.connect("mongodb://localhost:27017/")
+mongoose.connect("mongodb://localhost:27017/ravenPay")
 
 const userSchema = new Schema({
-    firstName: String,
-    lastName: String,
-    password: String
+    userName: {
+        type: String,
+        required: true,
+        minLength: 3,
+        maxLength: 30,
+        lowercase: true,
+        unique: true,
+        trim: true,
+    },
+    firstName: {
+        type: String,
+        required: true,
+        maxLength: 50,
+        trim: true,
+    },
+    lastName: {
+        type: String,
+        required: true,
+        maxLength: 50,
+        trim: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 6,
+    }
 })
 
 const User = mongoose.model("User", userSchema)
